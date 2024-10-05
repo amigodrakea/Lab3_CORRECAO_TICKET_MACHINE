940978aee0ec39dd7c29646e565dd1c5e9201d2d
Bruno Higuti <amigodrakea@gmail.com>
2024-10-05 18:05:39 -0300

    Correção issue 7 - Aprimoramento da função de troco.

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -1,86 +1,29 @@
 package core;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
-/**
- *
- * @author Calebe de Paula Bianchini
- */
-class Troco {
+public class Troco {
 
-    protected PapelMoeda[] papeisMoeda;
+    private List<Integer> cédulas;
 
-    public Troco(int valor) {
-        papeisMoeda = new PapelMoeda[6];
-        int count = 0;
-        while (valor % 100 != 0) {
-            count++;
+    // Construtor que calcula o troco com base no saldo e nas cédulas disponíveis
+    public Troco(int saldo) {
+        cédulas = new ArrayList<>();
+        int[] papelMoeda = {100, 50, 20, 10, 5, 2};  // Cédulas disponíveis para o troco
+
+        // Calcula o troco dividindo o saldo pelas cédulas, do maior valor para o menor
+        for (int i = 0; i < papelMoeda.length; i++) {
+            while (saldo >= papelMoeda[i]) {
+                saldo -= papelMoeda[i];  // Subtrai o valor da cédula do saldo
+                cédulas.add(papelMoeda[i]);  // Adiciona a cédula à lista de troco
+            }
         }
-        papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
-        while (valor % 50 != 0) {
-            count++;
-        }
-        papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-        while (valor % 20 != 0) {
-            count++;
-        }
-        papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
-        while (valor % 10 != 0) {
-            count++;
-        }
-        papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
-        while (valor % 5 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
-        while (valor % 2 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(2, count);
     }
 
-    public Iterator<PapelMoeda> getIterator() {
-        return new TrocoIterator(this);
-    }
-
-    class TrocoIterator implements Iterator<PapelMoeda> {
-
-        protected Troco troco;
-
-        public TrocoIterator(Troco troco) {
-            this.troco = troco;
-        }
-
-        @Override
-        public boolean hasNext() {
-            for (int i = 6; i >= 0; i++) {
-                if (troco.papeisMoeda[i] != null) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        @Override
-        public PapelMoeda next() {
-            PapelMoeda ret = null;
-            for (int i = 6; i >= 0 && ret != null; i++) {
-                if (troco.papeisMoeda[i] != null) {
-                    ret = troco.papeisMoeda[i];
-                    troco.papeisMoeda[i] = null;
-                }
-            }
-            return ret;
-        }
-
-        @Override
-        public void remove() {
-            next();
-        }
+    // Retorna o troco como um iterator de cédulas
+    public Iterator<Integer> getCédulas() {
+        return cédulas.iterator();
     }
 }
